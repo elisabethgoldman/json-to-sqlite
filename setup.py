060498d89da399f1bfae914d206b7fa3b39d831f
Charles Czysz <czysz@uchicago.edu>
2022-01-27 10:59:05 -0600

    Python files updated

--- setup.py
+++ setup.py
@@ -1,27 +1,10 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(name = 'json-to-sqlite',
-      author = 'Jeremiah H. Savage',
-      author_email = 'jeremiahsavage@gmail.com',
-      version = 0.15,
-      description = 'convert to json to column based sqlite',
-      url = 'https://github.com/jeremiahsavage/json-to-sqlite',
-      license = 'Apache 2.0',
-      packages = find_packages(),
-      install_requires = [
-          'pandas',
-          'sqlalchemy'
-      ],
-      classifiers = [
-          'Development Status :: 3 - Alpha',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: Apache Software License',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 3',
-      ],
-      entry_points={
-          'console_scripts': ['json_to_sqlite=json_to_sqlite.__main__:main']
-          },
+setup(
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
 )
+
+# __END__
